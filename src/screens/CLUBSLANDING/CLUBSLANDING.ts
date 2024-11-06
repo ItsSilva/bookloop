df2b6adeb604b1d1c38a08c9e7f06d99f3fba735
Juan Silva <juanse909@hotmail.com>
2024-11-06 18:47:23 -0500

    refractor: filter to only bring cards that the user has

--- src/screens/CLUBSLANDING/CLUBSLANDING.ts
+++ src/screens/CLUBSLANDING/CLUBSLANDING.ts
@@ -87,7 +87,7 @@ class ClubsLanding extends HTMLElement {
                 clubCard.setAttribute(AttributeDiscoverLandingCards.image, club.image);
                 clubCard.setAttribute(AttributeDiscoverLandingCards.name, club.name);
                 clubCard.setAttribute(AttributeDiscoverLandingCards.members, club.members);
-                clubCard.setAttribute(AttributeDiscoverLandingCards.button, 'Join');
+                clubCard.setAttribute(AttributeDiscoverLandingCards.button, 'Remove');
                 
                 // Apply styles to the button
                 const button = clubCard.shadowRoot?.querySelector('.button') as HTMLButtonElement;
