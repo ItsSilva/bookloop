090cee1e7590169582ee5e0681054794ad16f11f
Juan Silva <juanse909@hotmail.com>
2024-11-08 12:33:17 -0500

    feat: addLikes function added to component not working

--- src/components/postComponent/post.ts
+++ src/components/postComponent/post.ts
@@ -1,6 +1,8 @@
 import '../postPopUp/postPopUp'
-import PostPopUp, { Attribute3} from '../../components/postPopUp/postPopUp';
-
+import PostPopUp, { Attribute3 } from '../../components/postPopUp/postPopUp';
+import { addLikes } from '../../utils/firebase'
+import { addLikesAction } from '../../store/actions';
+import { dispatch, appState, addObserver } from '../../store';
 
 export enum Attribute2 {
     'clubpic' = 'clubpic',
@@ -10,6 +12,7 @@ export enum Attribute2 {
     'comments' = 'comments',
     'author' = 'author',
     'desc' = 'desc',
+    'uid' = 'uid',
 };
 
 class Post extends HTMLElement {
@@ -22,10 +25,22 @@ class Post extends HTMLElement {
     usercomments: string[] = [];
     likes: number = 0;
     liked: boolean = false;
+    uid: string;
 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        addObserver(this);
+        this.uid = '';
+        this.clubpic = '';
+        this.clubname = '';
+        this.image = '';
+        this.comments = 0;
+        this.author = '';
+        this.desc = '';
+        this.usercomments = [];
+        this.likes = 0;
+        this.liked = false;
     }
 
     static get observedAttributes() {
@@ -37,94 +52,97 @@ class Post extends HTMLElement {
             case Attribute2.clubpic:
                 this.clubpic = newValue as string;
                 break;
-                case Attribute2.clubname:
-                    this.clubname = newValue as string;
-                    break;
-                    case Attribute2.image:
+            case Attribute2.clubname:
+                this.clubname = newValue as string;
+                break;
+            case Attribute2.image:
                 this.image = newValue as string;
                 break;
             case Attribute2.likes:
-                this.likes = newValue ? Number(newValue) : 0; // Convertimos a number
+                this.likes = newValue ? Number(newValue) : 0; // Convert to number
                 break;
-                case Attribute2.comments:
-                    this.comments = newValue ? Number(newValue) : 0; // Convertimos a number
-                    break;
-                    case Attribute2.author:
-                        this.author = newValue as string;
+            case Attribute2.comments:
+                this.comments = newValue ? Number(newValue) : 0; // Convert to number
                 break;
-                case Attribute2.desc:
-                    this.desc = newValue as string;
-                    break;
-                    default:
-                        break;
-                    }
-                    this.render();
-                }
-                
-                connectedCallback() {
-                    this.render();
-                    this.addComments(); // Agregar el evento para enviar comentarios
-                    this.addLikeHandler();
-                }
-                
-                render() {
-                    if (this.shadowRoot) {
-                        this.shadowRoot.innerHTML = `
-                        <link rel="stylesheet" href="../src/components/postComponent/post.css">
-                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-                        
-                        <section class="post--container">
-                        <div class="post--header">
-                        <img src="${this.clubpic}" alt="Club Picture" class="club-pic">
-                        <div class="club-info">
-                        <h2>${this.clubname}</h2>
-                        </div>
-                        </div>
-                        <img src="${this.image}" alt="Post Image" class="post-image">
-                        <div class="post--body">
-                        <div class="post--info">
-                        <button class="likes" id="likeButton">
-                        <i class="${this.liked ? 'fas fa-heart' : 'far fa-heart'}"></i> 
-                        ${this.likes}
-                        </button>
-                        <span class="comments"><i class="fas fa-comment"></i> ${this.usercomments.length}</span>
-                        </div>
-                        <div class="post--author">
-                        <h3>@${this.author}</h3>
-                        <p>${this.desc}</p>
-                        </div>
-                        </div>  
-                        <div class='container-inputs'>
-                        <div class="input-wrapper-inputtext">
-                        <i class="fa-solid fa-message" style="color: #999;"></i>
-                        <input type="text" placeholder="Say something!" name="textInput" id="commentInput">
-                        <button type="submit" id="sendComment">Send</button>
-                        </div>
-                        <div class="comments-list">
-                        ${this.usercomments.map(comment => `<p class="comment-item">@${this.author}: ${comment}</p>`).join('')}
-                        </div>
-                        </div>
-                        </section>
-                        `;
+            case Attribute2.author:
+                this.author = newValue as string;
+                break;
+            case Attribute2.desc:
+                this.desc = newValue as string;
+                break;
+            case Attribute2.uid:
+                this.uid = newValue as string;
+                break;
+            default:
+                break;
+        }
+        this.render();
+    }
 
-                        this.addImageClickHandler();
-            this.addLikeHandler();  // Reasociar el like después de renderizar
-            this.addComments();      // Reasociar los comentarios después de renderizar
+    connectedCallback() {
+        this.render();
+        this.addComments();
+        this.addLikeHandler();
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="../src/components/postComponent/post.css">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+                
+                <section class="post--container">
+                <div class="post--header">
+                <img src="${this.clubpic}" alt="Club Picture" class="club-pic">
+                <div class="club-info">
+                <h2>${this.clubname}</h2>
+                </div>
+                </div>
+                <img src="${this.image}" alt="Post Image" class="post-image">
+                <div class="post--body">
+                <div class="post--info">
+                <button class="likes" id="likeButton">
+                <i class="${this.liked ? 'fas fa-heart' : 'far fa-heart'}"></i> 
+                ${this.likes}
+                </button>
+                <span class="comments"><i class="fas fa-comment"></i> ${this.usercomments.length}</span>
+                </div>
+                <div class="post--author">
+                <h3>@${this.author}</h3>
+                <p>${this.desc}</p>
+                </div>
+                </div>  
+                <div class='container-inputs'>
+                <div class="input-wrapper-inputtext">
+                <i class="fa-solid fa-message" style="color: #999;"></i>
+                <input type="text" placeholder="Say something!" name="textInput" id="commentInput">
+                <button type="submit" id="sendComment">Send</button>
+                </div>
+                <div class="comments-list">
+                ${this.usercomments.map(comment => `<p class="comment-item">@${this.author}: ${comment}</p>`).join('')}
+                </div>
+                </div>
+                </section>
+                `;
+
+            this.addImageClickHandler();
+            this.addLikeHandler();
+            this.addComments();
         }
     }
-                
+
     addImageClickHandler() {
         const postImage = this.shadowRoot?.querySelector<HTMLImageElement>('.post-image');
 
         if (postImage) {
             postImage.addEventListener('click', (event) => {
                 event.stopPropagation();
-                
+
                 const postPopup = document.createElement('post-popup') as PostPopUp;
                 postPopup.setPopupData(
-                    this.usercomments,  // Primer parámetro que es un array
-                    this.clubname, 
-                    this.image, 
+                    this.usercomments,
+                    this.clubname,
+                    this.image,
                     this.clubpic,
                     this.author,
                     this.desc,
@@ -132,7 +150,6 @@ class Post extends HTMLElement {
                 );
                 document.body.appendChild(postPopup);
                 console.log("clicked");
-                 
             });
         }
     }
@@ -140,18 +157,18 @@ class Post extends HTMLElement {
     addComments() {
         const input = this.shadowRoot?.querySelector<HTMLInputElement>('#commentInput');
         const button = this.shadowRoot?.querySelector<HTMLButtonElement>('#sendComment');
-        
+
         if (input && button) {
             button.addEventListener('click', () => {
                 const commentText = input.value.trim();
                 if (commentText) {
                     this.usercomments.push(commentText);
                     this.comments = this.usercomments.length;
-                    this.render();  
+                    this.render();
                     input.value = '';
                 }
             });
-            
+
             input.addEventListener('keypress', (event) => {
                 if (event.key === 'Enter') {
                     button.click();
@@ -160,14 +177,26 @@ class Post extends HTMLElement {
         }
     }
 
-    addLikeHandler() {
+    async addLikeHandler() {
         const likeButton = this.shadowRoot?.querySelector<HTMLButtonElement>('#likeButton');
-        
-        if (likeButton) {
-            likeButton.addEventListener('click', (e) => {
+
+        if (likeButton && this.uid) {
+            likeButton.addEventListener('click', async (e) => {
+                e.stopPropagation();
+
                 this.toggleLike();
                 this.likes += this.liked ? 1 : -1;
-                this.render();  
+
+                try {
+                    await addLikes(this.uid, this.liked);
+                    dispatch(addLikesAction(this.uid, this.liked));
+                    this.render();
+                } catch (error) {
+                    console.error("Error adding likes:", error);
+                    this.toggleLike(); // Revert like state if there's an error
+                    this.likes += this.liked ? -1 : 1;
+                    this.render();
+                }
             });
         }
     }
@@ -176,10 +205,6 @@ class Post extends HTMLElement {
         this.liked = !this.liked;
     }
 }
-            
-            
-            
-            
-            
+
 customElements.define('post-component', Post);
 export default Post;
