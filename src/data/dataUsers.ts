6e2339ed356da8f5452589a2a57a4c2a5cdae765
Juan Silva <juanse909@hotmail.com>
2024-11-26 15:01:41 -0500

    feat: change information to default

--- src/data/dataUsers.ts
+++ src/data/dataUsers.ts
@@ -3,7 +3,7 @@ export const dataUsers = [
         uid: 0,
         background: '../src/assets/user/background/mauricio-santos-N1gFsYf9AI0-unsplash.jpg',
         userpic: '../src/assets/user/pics/dwayne-joe-LTY3S3mFHGY-unsplash.jpg',
-        name: 'Mara Volkov',
-        username: '@MaraVolkov',
+        name: 'Default User',
+        username: '@DefaultUser',
     }
 ]
