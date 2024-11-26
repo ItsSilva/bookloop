45247c5bce7ddba89b20c5837b717ac7b1429ec0
Juan Silva <juanse909@hotmail.com>
2024-11-26 11:40:05 -0500

    feat: update component for editing user profile added

--- src/screens/EDITPROFILE/EDITPROFILE.ts
+++ src/screens/EDITPROFILE/EDITPROFILE.ts
@@ -1,21 +1,17 @@
-import { dispatch } from '../../store/index';
+import { appState, dispatch } from '../../store/index';
 import { navigate } from '../../store/actions';
 import { Screens } from '../../types/store';
-import { registerUser } from '../../utils/firebase';
-
-
-
-const credentials = {
-    email: '',
-    password: '',
-    name: '',
-    userName: '',
-};
+import { updateProfile } from '../../utils/firebase';
 
 class EditProfile extends HTMLElement {
+    editedProduct: any;
+
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.editedProduct = appState.userData;
+        this.changeName = this.changeName.bind(this);
+        this.changeUserName = this.changeUserName.bind(this);
     }
 
     connectedCallback() {
@@ -23,16 +19,19 @@ class EditProfile extends HTMLElement {
     }
 
     changeName(e: any) {
-        credentials.name = e.target.value;
+        const input = e.target as HTMLInputElement;
+        this.editedProduct.name = input.value;
     }
 
-    changeAge(e: any) {
-        credentials.userName = e.target.value;
+    changeUserName(e: any) {
+        const input = e.target as HTMLInputElement;
+        this.editedProduct.userName = input.value;
     }
 
     async submitForm() {
-        const resp = await registerUser(credentials);
-        resp ? dispatch(navigate(Screens.PROFILE)) : alert('Your personal information could not be changed. Excuse us :(');
+        console.log('Edited Product for FB', this.editedProduct);
+        dispatch(updateProfile(this.editedProduct));
+        dispatch(navigate(Screens.PROFILE));
     }
 
     redirectToLogin() {
@@ -65,35 +64,31 @@ class EditProfile extends HTMLElement {
 
             // Form fields
             const pName = document.createElement('input');
-            pName.placeholder = 'Full name';
+            pName.placeholder = 'update your name';
             pName.className = 'form-input';
             pName.required = true;
             pName.addEventListener('change', this.changeName);
             form.appendChild(pName);
 
             const pUserName = document.createElement('input');
-            pUserName.placeholder = 'User name';
+            pUserName.placeholder = 'update user name';
             pUserName.className = 'form-input';
             pUserName.required = true;
-            pUserName.addEventListener('change', this.changeAge);
+            pUserName.addEventListener('change', this.changeUserName);
             form.appendChild(pUserName);
 
-            // Registration button
+            // Edit button
             const save = document.createElement('button');
             save.innerText = 'Edit Profile';
             save.className = 'form-button';
-            // save.addEventListener('click', this.submitForm);
+            save.addEventListener('click', this.submitForm);
             form.appendChild(save);
 
             // Profile button
             const profile = document.createElement('button');
             profile.innerText = 'Return to profile';
             profile.className = 'form-button';
-
-            // CHANGE BY FUNCTION THAT MODIFIES USER INFORMATION
             profile.addEventListener('click', this.redirectToLogin);
-            // CHANGE BY FUNCTION THAT MODIFIES USER INFORMATION
-
             form.appendChild(profile);
 
 
